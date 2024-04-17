980bd6ea92571598b6133ae51024ebc6e520b620
Adrian Dudek <adrian.dudek02@wp.pl>
2024-04-17 22:40:46 +0200

    Adding json file, connecting it to hmtl

---
+++ code.js
@@ -0,0 +1,25 @@
+function writeDown(){
+    fetch('./carsinfo.json')
+    .then(response => response.json())
+    .then(data => {
+
+        let htmlCars = document.getElementById('cars');
+        const cars = data.Cars;
+
+        cars.forEach(car => { 
+            htmlCars.innerHTML += `
+            <div class="post">
+            <a href="${car.GoTo}">
+                <img src="${car.ImagePath}">
+                <h2>${car.Name}</h2>
+                <p>${car.Description}</p>
+            </a>
+            </div>`;  
+        });
+    })
+    .catch(error => {
+        console.error('Wystąpił błąd podczas wczytywania pliku JSON:', error);
+    });
+}
+
+writeDown();
